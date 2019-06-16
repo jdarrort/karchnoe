1a72e21584cf69c161828bcb38927e60ce2058d2
Julien Darrort <julien@darrort.com>
2019-06-16 18:49:30 +0200

    browserfix

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const path = require('path');
 var app = express();
 
 
-const C_SERVER_PORT = 80
-const C_KARCH_REPO = "karch_repo"
+const C_SERVER_PORT = process.env["KARCH_PORT"] || 80 ;
+const C_KARCH_REPO = "karch_repo" ; 
 
 global.appRoot = path.resolve(__dirname);
 global.repoRoot = path.join(path.resolve(__dirname), C_KARCH_REPO);
